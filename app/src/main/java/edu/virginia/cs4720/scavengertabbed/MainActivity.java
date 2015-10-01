5db5d29d534e2201d5e431182f66eefbc2bbe08f
Katie Banach <kab5vv@virginia.edu>
2015-09-30 20:30:06 -0400

    Image Upload

--- app/src/main/java/edu/virginia/cs4720/scavengertabbed/MainActivity.java
+++ app/src/main/java/edu/virginia/cs4720/scavengertabbed/MainActivity.java
@@ -4,10 +4,12 @@ import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.support.design.widget.TabLayout;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,9 +18,11 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
@@ -35,6 +39,8 @@ public class MainActivity extends AppCompatActivity {
 
     Double latitude;
     Double longitude;
+
+    ImageView imgView;
 
     Calendar myCalendar;
     EditText dateEditText;
@@ -131,6 +137,18 @@ public class MainActivity extends AppCompatActivity {
 
         };
 
+        imgView = (ImageView) findViewById(R.id.imageUploadView);
+
+        Button imageButton = (Button) findViewById(R.id.uploadImageButton);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent imageIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(imageIntent, 0);
+            }
+        });
+
+
         events = new ArrayList<>(Event.findWithQuery(Event.class, "SELECT * from Event"));
 
         if (events.isEmpty()) {
@@ -157,6 +175,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
 
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+        imgView.setImageBitmap(bitmap);
     }
 
     private void setupViewPager(ViewPager viewPager) {
